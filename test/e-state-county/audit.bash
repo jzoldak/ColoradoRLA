be8ffd841c760b71c47303a9613854bce4df3c2c
Neal McBurnett <nealmcb@freeandfair.us>
2018-10-17 20:16:52 -0600

    Add CROPTS for crtest options like --debuglevel
    now that the default is so quiet

--- test/e-state-county/audit.bash
+++ test/e-state-county/audit.bash
@@ -12,6 +12,8 @@
 LOGDIR=${LOGDIR:-/tmp}
 LOGTAG=${LOGTAG:-state-county}
 
+CROPTS=${CROPTS:-"-d 20"}
+
 mkdir -p $LOGDIR
 
 (
@@ -19,15 +21,15 @@ mkdir -p $LOGDIR
     ./main.py reset
 
     echo -e "\nStart 'Define audit' step and upload county audit data\n"
-    ./main.py dos_init -r 0.05
+    ./main.py $CROPTS dos_init -r 0.05
 
-    ./main.py -c 1 -f ../e-state-county/cvr-1000--20-200.csv -F ../e-state-county/manifest-1-1000.csv county_setup
-    ./main.py -c 2 -f ../e-state-county/cvr-10--0--2-4-1.csv -F ../e-state-county/manifest-2-10.csv county_setup
-    ./main.py -c 3 -f ../e-state-county/cvr-100--2-24-40.csv -F ../e-state-county/manifest-3-100.csv county_setup
+    ./main.py $CROPTS -c 1 -f ../e-state-county/cvr-1000--20-200.csv -F ../e-state-county/manifest-1-1000.csv county_setup
+    ./main.py $CROPTS -c 2 -f ../e-state-county/cvr-10--0--2-4-1.csv -F ../e-state-county/manifest-2-10.csv county_setup
+    ./main.py $CROPTS -c 3 -f ../e-state-county/cvr-100--2-24-40.csv -F ../e-state-county/manifest-3-100.csv county_setup
 
     echo -e "\nFinish 'Define audit' step to select contests, enter the random seed, and Launch the Audit\n"
 
-    ./main.py -C 0 -C 1 -C 4 -C 5 -s 12341234123412341234 dos_start
+    ./main.py $CROPTS -C 0 -C 1 -C 4 -C 5 -s 12341234123412341234 dos_start
 
 ) | tee $LOGDIR/$LOGTAG.sm
 
@@ -36,9 +38,9 @@ rla_export -e $LOGDIR/$LOGTAG-setup.export
 (
     echo -e "\nRun first round, with one discrepancy in county 1 (Adams)\n"
 
-    ./main.py -c 1 -p '1 100' -R 1 county_audit
-    ./main.py -c 2 -p '-1 100' -R 1 county_audit
-    ./main.py -c 3 -p '-1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 1 -p '1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 2 -p '-1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 3 -p '-1 100' -R 1 county_audit
 ) | tee -a $LOGDIR/$LOGTAG.sm
 
 rla_export -e $LOGDIR/$LOGTAG-round1.export
@@ -49,9 +51,9 @@ read
 (
     echo -e "\nRun second round\n"
 
-    ./main.py -c 1 -p '-1 100' -R 1 county_audit
-    ./main.py -c 2 -p '-1 100' -R 1 county_audit
-    ./main.py -c 3 -p '-1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 1 -p '-1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 2 -p '-1 100' -R 1 county_audit
+    ./main.py $CROPTS -c 3 -p '-1 100' -R 1 county_audit
 ) | tee -a $LOGDIR/$LOGTAG.sm
 
 rla_export -e $LOGDIR/$LOGTAG-round2.export
