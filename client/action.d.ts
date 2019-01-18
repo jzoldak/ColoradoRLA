df994462a23782975152782efde0e893ef2d3e79
Gordon Stratton <gordon@democracy.works>
2019-01-18 22:57:25 +0000

    Remove audit board state workaround
    This was a really ugly hack while implementing multiple audit boards
    (and, frankly, learning redux) that we can pretty easily remove with
    some more redux knowledge.

--- client/action.d.ts
+++ client/action.d.ts
@@ -25,6 +25,7 @@ declare namespace Action {
         | ImportCvrExportOk
         | Login1FOk
         | SelectContestsForAuditOk
+        | SetAuditBoard
         | UpdateAcvrForm
         | UploadBallotManifestOk
         | UploadAcvrFail
@@ -134,6 +135,13 @@ declare namespace Action {
     interface SelectContestsForAuditOk {
         type: 'SELECT_CONTESTS_FOR_AUDIT_OK';
         data: any;
+    }
+
+    interface SetAuditBoard {
+        type: 'SET_AUDIT_BOARD';
+        data: {
+            auditBoardIndex: number;
+        };
     }
 
     interface UpdateAcvrForm {
